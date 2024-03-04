a4f5d4120b0556098f33e28d0879a9d2a7cabcf8
tilok <tilakthexp@gmail.com>
2024-03-04 11:30:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.status(200).send({ message: "hi I'm up" });
+});
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
